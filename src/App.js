35a90ff6f4bb21af3b93db9a592d99ced5e59acc
Shyamal-Mishra <mishra.shyamal116@gmail.com>
2024-09-08 14:18:35 -0400

    adding proxy

--- src/App.js
+++ src/App.js
@@ -6,14 +6,40 @@ import Footer from "./UI/Footer";
 import Header from "./component/Header";
 import AllRoutes from "./component/AllRoutes";
 import Bot from "./component/bot";
-import Login from "./UI/LogIn/Login";
 import { AuthContext } from "./component/AuthContextProvider";
-import Home from "./UI/Home"
-
+import Home from "./UI/Home";
 
 function App() {
   useEffect(() => {
     Aos.init();
+
+    // Function to dynamically load CSS files
+    const loadCss = () => {
+      const cssBaseUrl = process.env.REACT_APP_CSS_BASE_URL;
+      const cssFiles = [
+        'header.css',       // Path to your main CSS file
+        'bot.css', 
+        'footer.css', 
+        'hero.css', 
+        'pricing.css', 
+        'Questionnaires.css', 
+        'exercise.css',
+        'modal.css',
+        'start.css'  ,
+        'ReportTracker.css',
+        'track.css' // Path to additional CSS files
+      ];
+
+      cssFiles.forEach((cssFile) => {
+        const linkElement = document.createElement('link');
+        linkElement.rel = 'stylesheet';
+        linkElement.href = `${cssBaseUrl}${cssFile}`;
+        document.head.appendChild(linkElement);
+      });
+    };
+
+    // Load CSS when component mounts
+    loadCss();
   }, []);
 
   const { user } = useContext(AuthContext);
